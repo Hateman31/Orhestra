63ec9558fc423b335c947fee91b2e39649b6b187
hateman31 <metilogrunge31@gmail.com>
2016-07-06 17:14:09 +0300

    del import window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.core.window import Window
 
 class Root(BoxLayout):
 	pass
@@ -9,5 +8,6 @@ class MainApp(App):
 		def build(self):
 			main = Root()
 			return main
+
 if __name__ == '__main__':
 	MainApp().run()			
